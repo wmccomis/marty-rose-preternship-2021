028b77919dccf6c18368e7b08fb05e87a7b61a19
Luke Marushack <lmarusha@nd.edu>
2021-04-27 23:05:25 -0400

    Queue: v2

--- importance.py
+++ importance.py
@@ -6,12 +6,12 @@
 
 def get_importance(run_time, urgency, estimated_cores):
     # Run Time: 1-5, 5 longest
-    # Urgency: 1-5, 5 highest
+    # Urgency: 1-10, 5 highest
     # Cores: 1-4, 4 highest
     # Most Importance = few cores, high urgency, low runtime. Those go first.
     rt_mult = 3
-    urgency_mult = 5
+    urgency_mult = 3 
     cores_mult = 2
 
-    importance = rt_mult*(6 - run_time) + urgency_mult*urgency + cores_mult * (5 - estimated_cores)
+    importance = rt_mult*(6 - int(run_time)) + urgency_mult*int(urgency) + cores_mult * (5 - int(estimated_cores))
     return importance
--- overflow.py
+++ overflow.py
@@ -6,23 +6,37 @@
 
 from queue import Queue
 from time import sleep
+import json
+
+from receiver import *
+from importance import *
+from sender import *
 
 def format_task(name, cores, server):
     return  {"Command" : name, "Cores" : cores, "Server" : server}
 
-def process_jobs():
+def process_jobs(filename):
     q = Queue()
-    """
-    Still need Luis' core estimation code and the code that tells this program
-    what jobs are about to be run. Then I can finish this script. 
-    """
+
+    data = json.load(open(filename, "r"))
+    for key in data.keys():
+        data[key]["importance"] = get_importance(data[key]["approximate_run_time"], data[key]["urgency"], data[key]["core_estimation"])
+
+    data = dict(sorted(data.items(), reverse = True, key = lambda process: process[1]["importance"]))
+    for item in data.keys():
+        q.put(data[item])
+
     while (not q.empty()):
-        job = q.front()
-        est_cores = estimated_cores(job)
-        serverID, cores_used = handle_job(summedData(), est_cores)
+        curr_job = q.queue[0]
+        serverID, cores_used = handle_job(summedData(), curr_job["core_estimation"])
         if (serverID == -1):
             sleep(0.5)
             continue
-        jobdata = format_task(job[name], cores_used, serverID)
+        jobdata = format_task(curr_job["name"], cores_used, serverID)
         sender(jobdata, serverID)
-        q.pop()
+        q.get()
+
+"""
+Testing:
+process_jobs("data.json")
+"""
